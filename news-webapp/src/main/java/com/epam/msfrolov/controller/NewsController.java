291968e32fc02d0afc031b64b74e8421ebeca60c
Maxim_Frolov <Maxim_Frolov@epam.com>
2016-07-05 10:38:41 +0600

    Closes origin/master news#7

--- news-webapp/src/main/java/com/epam/msfrolov/controller/NewsController.java
+++ news-webapp/src/main/java/com/epam/msfrolov/controller/NewsController.java
@@ -89,6 +89,9 @@ public class NewsController {
         //efgwergerg
         //efgwergerg
         //efgwergerg
+        //efgwergerg
+        //efgwergerg
+        //efgwergerg
         return PAGE_NEWS_LIST;
     }
 }
